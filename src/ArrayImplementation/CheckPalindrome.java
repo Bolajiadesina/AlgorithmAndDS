c40aae784aded4f3baab91f735176ca871f17449
Bolaji Adesina <bradesin@bradford.ac.uk>
2023-09-15 19:35:11 +0100

    adding palindrome check

---
+++ src/ArrayImplementation/CheckPalindrome.java
@@ -0,0 +1,33 @@
+package ArrayImplementation;
+
+public class CheckPalindrome {
+    
+    private static Boolean check(){
+            String word="madam";//#endregion
+
+            String rev="";
+
+            for(int i=word.length()-1; i>=0; i--){
+
+               rev= rev+word.charAt(i);
+            
+
+            }
+
+            if(word.equals(rev)){
+                return true;
+            }
+
+
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(check());
+    }
+
+
+
+}
