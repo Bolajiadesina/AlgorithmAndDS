30c264e91df06a95416b41dff92cca27c34761f9
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-10-26 23:47:58 +0100

    add two numbers singly linked list

--- src/ArrayImplementation/AddNodeToSinglyList.java
+++ src/ArrayImplementation/AddNodeToSinglyList.java
@@ -292,6 +292,32 @@ public class AddNodeToSinglyList {
     }
 
 
+    public ListNode addTwoNumbers(ListNode a,ListNode b){
+        ListNode dummy= new ListNode(0);
+        ListNode tail = dummy;
+        int carry=0;
+
+        while(a !=null || b!=null){
+            int x = (a !=null) ? a.data : 0;
+            int y = (b !=null) ? b.data : 0;
+
+            int sum = carry+x+y;
+
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+
+            if(a !=null) a= a.next;
+            if(b !=null) b= b.next;
+
+        }
+
+        if(carry> 0){
+            tail.next = new ListNode(carry);
+        }
+
+        return dummy.next;
+    }
+
 
 
     public static void main(String[] args) {
