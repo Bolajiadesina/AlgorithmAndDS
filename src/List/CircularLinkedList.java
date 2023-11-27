c2623b9d54d8940df7ce304978b88c0642fb8ed8
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-11-27 22:09:21 +0000

    circular adding printing

--- src/List/CircularLinkedList.java
+++ src/List/CircularLinkedList.java
@@ -1,14 +1,14 @@
 package List;
 
-public class CircularLinkedList {
-    private ListNode last;
-    private int  lenght;
+// public class CircularLinkedList {
+//     private ListNode last;
+//     private int  lenght;
 
-    public ListNode(int data){
-        private ListNode next;
-        private int data;
+//     public ListNode(int data){
+//         private ListNode next;
+//         private int data;
 
-        public ListNode(int data){
-        this.data=data;
-        }
-    }
+//         public ListNode(int data){
+//         this.data=data;
+//         }
+//     }
