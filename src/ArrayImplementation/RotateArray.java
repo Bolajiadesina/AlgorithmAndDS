3a6c29c060798f9612cb2fe7959ce254260d6404
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-12-24 19:16:12 +0000

    bubble rote

--- src/ArrayImplementation/RotateArray.java
+++ src/ArrayImplementation/RotateArray.java
@@ -1,8 +1,12 @@
 package ArrayImplementation;
 
-public class RotateArray {
+import java.util.Arrays;
+import java.util.logging.Logger;
 
-    private static void rotate(int[] nums, int k) {
+public class RotateArray {
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    private  void rotate(int[] nums, int k) {
         if (k > nums.length) {
             k = k % nums.length;
         }
@@ -11,21 +15,27 @@ public class RotateArray {
 
         for (int i = 0; i < k; i++) {
             result[i] = nums[nums.length - k + i];
+        
+            logger.info(Arrays.deepToString(result) );
+            
         }
 
         int j = 0;
         for (int i = k; i < nums.length; i++) {
             result[i] = nums[j];
             j++;
+          
+            logger.info(result.toString());
         }
         System.arraycopy(result, 0, nums, 0, nums.length);
     }
 
     public static void main(String[] args) {
+        RotateArray rotateArray= new RotateArray();
         int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
         int kp = 3;
 
-        rotate(arr, kp);
+        rotateArray.rotate(arr, kp);
 
     }
 }
