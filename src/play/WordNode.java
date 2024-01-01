2c3deb5f063015b13c68eb0a4d95161a3d58bf10
Bolajiadesina <adesina.bolaji@yahoo.com>
2024-01-01 16:24:24 +0000

    word node add comment

--- src/play/WordNode.java
+++ src/play/WordNode.java
@@ -1,7 +1,9 @@
 package play;
 
 public class WordNode {
-    
+    /*
+     * word  and the number of steps required
+    */
         String word;
         int numSteps;
         public WordNode(String word, int numSteps){
