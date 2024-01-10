d173df70330fe05563c7088985a21b8d44cf1253
Bolajiadesina <adesina.bolaji@yahoo.com>
2024-01-10 23:41:51 +0000

    closest positve number

---
+++ src/play/ClosestPositiveNumber.java
@@ -0,0 +1,20 @@
+package play;
+
+public class ClosestPositiveNumber {
+
+
+    public int findClosest(){
+        int result=0;
+
+
+        return result;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        ClosestPositiveNumber cl= new ClosestPositiveNumber();
+    }
+    
+}
